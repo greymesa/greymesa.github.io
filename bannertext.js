c4f78da3ea090882734eaa5aa0204ae48a55e82e
greymesa <58270183+greymesa@users.noreply.github.com>
2023-01-23 19:45:22 -0500

    reset

--- bannertext.js
+++
@@ -1,7 +0,0 @@
-fetch("/lines.json")
-.then(res => res.json())
-.then(json => {
-    document.getElementById("banner").innerHTML = json[Math.floor(Math.random() * json.length)];
-}).catch(error => {
-    console.log(error);
-});
